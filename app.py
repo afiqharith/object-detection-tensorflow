36bdab02db4a43fb984c483b25bf8bb1737f80f2
Afiq Harith <afiqharith05@gmail.com>
2021-03-17 20:53:57 +0800

    xx

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with open(COCO_NAMEPATH, "r") as f:
 class App:
     def __init__(self, VIDEOPATH):
         self.video = cv2.VideoCapture(VIDEOPATH)
-        self.modelpath = MODELPATH
+        self.model = Model(MODELPATH)
         self.main()
 
     def get_box_detection(self,boxes,scores,classes,height,width):
@@ -23,8 +23,6 @@ class App:
 
     def main(self):
         
-        model = Model(self.modelpath)
-
         while True:
             self.flag, self.frame = self.video.read()
             if self.flag:
@@ -32,9 +30,9 @@ class App:
             else:
                 break
 
-            (boxes, scores, classes) =  model.predict(self.frame)
-            detectedBox = self.get_box_detection(boxes,scores[0].tolist(),classes[0].tolist(),self.frame.shape[0],self.frame.shape[1])
-            for bbox in detectedBox:
+            (boxes, scores, classes) =  self.model.predict(self.frame)
+            detected_bbox = self.get_box_detection(boxes,scores[0].tolist(),classes[0].tolist(),self.frame.shape[0],self.frame.shape[1])
+            for bbox in detected_bbox:
                 ymin = bbox[0]
                 xmin = bbox[1]
                 ymax = bbox[2]
@@ -42,7 +40,7 @@ class App:
                 objectname = bbox[4]
                 percent = int(bbox[5]*100)
 
-                cv2.rectangle(self.frame, (xmin, ymin), (xmax, ymax), BLACK, 2)
+                cv2.rectangle(self.frame, (xmin, ymin), (xmax, ymax), BLACK, 1)
 
                 label = f"{objectname} {'%.1d' % percent}%"
                 labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
@@ -50,7 +48,7 @@ class App:
                 cv2.rectangle(self.frame, (xmin, y1label - labelSize[1]),(xmin + labelSize[0], ymin + baseLine), BLACK, cv2.FILLED)
                 cv2.putText(self.frame, label, (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 1, cv2.LINE_AA)
 
-            cv2.imshow("OD", self.frame)
+            cv2.imshow("Object Detection FRCNN Inception MSCOCO", self.frame)
             if cv2.waitKey(1) & 0xff == ord('q'):
                 break
 
--- setup/config.py
+++ setup/config.py
@@ -1,8 +1,8 @@
 import os
 
-VIDEOPATH = os.path.join(os.getcwd(), 'videos/', 'TownCentre.mp4')
-MODELPATH = os.path.join(os.getcwd(),'models/','faster_rcnn_inception_v2_coco_2018_01_28/','frozen_inference_graph.pb')
-COCO_NAMEPATH = os.path.join(os.getcwd(), 'models/', 'coco.names')
+VIDEOPATH = os.path.join(os.getcwd(), 'videos', 'TownCentre.mp4')
+MODELPATH = os.path.join(os.getcwd(),'models','faster_rcnn_inception_v2_coco_2018_01_28','frozen_inference_graph.pb')
+COCO_NAMEPATH = os.path.join(os.getcwd(), 'models', 'coco.names')
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
